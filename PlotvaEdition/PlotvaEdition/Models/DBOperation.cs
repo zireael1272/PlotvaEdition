674fad997612e1101887514aec941a85b391465b
zireael1272 <kseniagonsharenko2007@gmail.com>
2024-10-19 23:27:40 +0300

    Update
    i wanna cry....

---
+++ PlotvaEdition/PlotvaEdition/Models/DBOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlotvaEdition.Models
+{
+    internal class DBOperation
+    {
+        private SqlConnection sqlConnection = null;
+        DBOperation()
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DeliveryDB"].ConnectionString);
+        }
+
+    }
+}
